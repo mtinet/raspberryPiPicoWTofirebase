cf64fab097b201e29fe0c928a64778609e29d6df
Jason Juhyun Kim <mtinet@hanmail.net>
2022-12-04 21:49:58 +0900

    Create basicRepeat.py

---
+++ basicRepeat.py
@@ -0,0 +1,28 @@
+from machine import Pin, I2C
+import network
+import time
+import urequests
+
+# Connect Internet by WiFi
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect("와이파이 이름", "와이파이 비번")
+print(wlan.isconnected())
+print(wlan.ifconfig())
+
+url = "파이어베이스 리얼타임 데이터베이스 주소"
+
+# DB 내역 가져오기
+response = urequests.get(url+".json")
+print(response.content)
+
+while True:
+    # 객체 교체하기, 특정 주소의 데이터가 변경됨
+    myobj = {'humi': '43'}
+    urequests.patch(url+"smartFarm.json", json = myobj)
+    time.sleep(5)
+    myobj = {'humi': '24'}
+    urequests.patch(url+"smartFarm.json", json = myobj)
+    time.sleep(5)
+
+
