9d9fe432ed38cbb9c7724a0a739f9ac8f631f962
Jason Juhyun Kim <mtinet@hanmail.net>
2022-12-07 00:37:24 +0900

    Update basicRepeat.py

--- basicRepeat.py
+++ basicRepeat.py
@@ -15,20 +15,20 @@ url = "파이어베이스 리얼타임 데이터베이스 주소"
 # DB 내역 가져오기
 response = urequests.get(url+".json")
 print(response.content)
-response.close()
+response.close() # byte형식의 결과물은 항상 닫아주어야 메모리 관리가 됨
 
 while True:
     # 객체 교체하기, 특정 주소의 데이터가 변경됨
     myobj = {'humi': '43'}
     response = urequests.patch(url+"smartFarm.json", json = myobj)
     response
-    response.close()
+    response.close() # byte형식의 결과물은 항상 닫아주어야 메모리 관리가 됨
     time.sleep(5)
     
     myobj = {'humi': '24'}
     response = urequests.patch(url+"smartFarm.json", json = myobj)
     response
-    response.close()
+    response.close() # byte형식의 결과물은 항상 닫아주어야 메모리 관리가 됨
     time.sleep(5)
 
 
