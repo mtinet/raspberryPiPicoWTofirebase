2fbe1d0c3cd6927f020544432f7c134de1bf8477
Jason Juhyun Kim <mtinet@hanmail.net>
2022-12-07 00:33:28 +0900

    Update basicRandom.py

--- basicRandom.py
+++ basicRandom.py
@@ -14,14 +14,17 @@ print(wlan.ifconfig())
 url = "https://smartfarm-f867f-default-rtdb.firebaseio.com/"
 
 # DB 내역 가져오기
-response = urequests.get(url+".json")
-print(response.content)
-response.close()
+response = urequests.get(url+".json").json()
+# print(response)
+# print(response['smartFarm'])
+# print(response['smartFarm']['led'])
 
 while True:
+    response = urequests.get(url+".json")
+    response.close()
     # 객체 교체하기, 특정 주소의 데이터가 변경됨
-    myobj = {'humi': random.randrange(1,100)}
-    r = urequests.patch(url+"smartFarm.json", json = myobj)
-    r
-    r.close()
+    myobj = {'humi': random.randrange(0,100), 'temp': random.randrange(0, 50)}
+    request = urequests.patch(url+"smartFarm.json", json = myobj)
+    request
+    request.close()
     time.sleep(1)
