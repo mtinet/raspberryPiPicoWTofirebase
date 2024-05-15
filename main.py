6ed41b7bf650153334790ee710477dd52ba91a45
Jason Juhyun Kim <mtinet@hanmail.net>
2024-05-15 21:23:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -28,24 +28,28 @@ def wifiConnect():
 # 와이파이 함수 실행
 wifiConnect()
 
+led = Pin(22, Pin.OUT)
+
 url = "자신의 파이어베이스 리얼타임 데이터베이스 주소를 넣을 것"
 
 # DB 내역 가져오기
 response = urequests.get(url+".json").json()
 # byte형태의 데이터를 json으로 변경했기 때문에 메모리를 닫아주는 일을 하지 않아도 됨 
-# print(response)
+print(response)
 # print(response['smartFarm'])
 # print(response['smartFarm']['led'])
+
+# 객체 초기화, 'led'를 '0'으로 설정함.
+led.value(0)
+myobj = {'led': 0}
+urequests.patch(url+".json", json = myobj)
+
 
 while True:
     # 현재 DB의 정보를 가져옴
     response = urequests.get(url+".json").json()
     # 현재 DB의 led 키 값의 상태에 따라 led 27번을 제어
-    if (response['smartFarm']['led'] == 0) :
+    if (response['led'] == 0) :
         led.value(0)
     else :
         led.value(1)
-        
-    # 객체 교체하기, 특정 주소의 데이터가 변경됨
-    myobj = {'humi': random.randrange(0,100), 'temp': random.randrange(0, 50)}
-    urequests.patch(url+"smartFarm.json", json = myobj).json()
