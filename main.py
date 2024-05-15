53579813415d5896b0413493a4b124abd2c622bc
Jason Juhyun Kim <mtinet@hanmail.net>
2024-05-15 21:07:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,29 @@ import time
 import urequests
 import random
 
-# 제어할 핀 번호 설정
-from machine import Pin
-led = Pin(27, Pin.OUT)
+# 와이파이 정보 
+SSID = 'U+Net03CC'
+password = 'J6FDFE#490'
 
 # 와이파이 연결하기
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(secrets.SSID, secrets.PASSWORD)
-print(wlan.isconnected())
-print(wlan.ifconfig())
+def wifiConnect():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        # 와이파이 연결하기
+        wlan.connect(SSID, password)  # 5, 6번 줄에 입력한 SSID와 password가 입력됨
+        print("Waiting for Wi-Fi connection", end="...")
+        print()
+        while not wlan.isconnected():
+            print(".", end="")
+            time.sleep(1)
+    else:
+        print(wlan.ifconfig())
+        print("WiFi is Connected")
+        print()
+
+# 와이파이 함수 실행
+wifiConnect()
 
 url = "자신의 파이어베이스 리얼타임 데이터베이스 주소를 넣을 것"
 
